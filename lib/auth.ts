71c20db881b77c7cf3eefdb80f88849654fcd9ca
kiralightyagami <crucisgamer98@gmail.com>
2025-12-17 17:39:35 +0530

    forgot to save

--- lib/auth.ts
+++ lib/auth.ts
@@ -6,4 +6,10 @@ export const auth = betterAuth({
     database: prismaAdapter(prisma, {
         provider: "postgresql", // or "mysql", "postgresql", ...etc
     }),
+    socialProviders: {
+        github: {
+            clientId: process.env.GITHUB_CLIENT_ID!,
+            clientSecret: process.env.GITHUB_CLIENT_SECRET!,
+        },
+    },
 });
